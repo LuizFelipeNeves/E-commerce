88e1549e161a6a8fab08440d93c3ca4df8dcec06
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-12-14 02:06:03 -0300

    Adding Multiple Items To Cart

--- src/redux/cart/reducer.jsx
+++ src/redux/cart/reducer.jsx
@@ -1,21 +1,22 @@
+import { addItemToCart } from './utils'
+
 const initialState = {
-    hidden: true,
-    cartItems: []
+	hidden: true,
+	cartItems: []
 }
 
 const useReducer = (state = initialState, action) => {
-    const actionstype = {
-        'TOGGLE_CART_HIDDEN': {
-            ...state,
-            hidden: !state.hidden
-
-        },
-        'ADD_ITEM': {
-            ...state,
-            cartItems: [...state.cartItems, action.payload]
-        }
-    }
-    return actionstype[action.type] || state
+	const actionstype = {
+		TOGGLE_CART_HIDDEN: {
+			...state,
+			hidden: !state.hidden
+		},
+		ADD_ITEM: {
+			...state,
+			cartItems: addItemToCart(state.cartItems, action.payload)
+		}
+	}
+	return actionstype[action.type] || state
 }
 
 export default useReducer
---
+++ src/redux/cart/utils.js
@@ -0,0 +1,13 @@
+export const addItemToCart = (cartItems, cartItemtoAdd) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemtoAdd.id
+	)
+
+	return existingCartItem
+		? cartItems.map((cartItem) =>
+				cartItem.id === cartItemtoAdd.id
+					? { ...cartItem, quantity: cartItem.quantity + 1 }
+					: cartItem
+		  )
+		: [...cartItems, { ...cartItemtoAdd, quantity: 1 }]
+}

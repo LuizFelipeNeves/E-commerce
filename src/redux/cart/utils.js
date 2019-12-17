7e5f91846f01ee801d7f286ae66b048e0478b83d
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-12-16 22:27:24 -0300

    Fixed bug on add multiple cart itens

--- src/redux/cart/utils.js
+++ src/redux/cart/utils.js
@@ -3,12 +3,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 		const existingCartItem = cartItems.find(
 			(cartItem) => cartItem.id === cartItemToAdd.id
 		)
-		if (existingCartItem)
+		return existingCartItem ? 
 			cartItems.map((cartItem) =>
 				cartItem.id === cartItemToAdd.id
 					? { ...cartItem, quantity: cartItem.quantity + 1 }
 					: cartItem
-			)
+			): [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 	}
-	return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+	return cartItems
 }

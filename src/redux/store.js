a1fa61ca6adbe1cc08d921471ff070ae3d432a10
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-12-25 04:35:44 -0300

    Optimizing Production Build with dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
-import logger from 'redux-logger'
 
 import rootReducer from './rootReducer'
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger')
+
+  middlewares.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 const persistor = persistStore(store)
